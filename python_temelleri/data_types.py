94b4e516e65a2ee621593e8dbc4e436c328269a2
Büşra Sümeyye <84714922+busrasumeyye@users.noreply.github.com>
2022-10-09 19:13:28 +0300

    Update data_types.py

--- python_temelleri/data_types.py
+++ python_temelleri/data_types.py
@@ -1,4 +1,7 @@
 
 print('5')
-print(2+2.3)
-print(2+2.0)
+print(2+2.3) #<class 'int'>
+print(2+2.0) #<class 'int'>
+print(type(5.0)) #<class 'float'>
+print(type(2+2)) #<class 'int'>
+print(type(7.1)) #<class 'float'>
