9250dca83dc2da942c544b36a5c6cfbffad73f55
Büşra Sümeyye <84714922+busrasumeyye@users.noreply.github.com>
2022-10-09 20:54:07 +0300

    Add files via upload

---
+++ python_temelleri/variables.py
@@ -0,0 +1,33 @@
+urunA = 5000
+urunB = 6000
+urunC = 7000
+kdv = 0.18
+print(5000 +(5000*0.18))
+print(6000 +(6000*0.18))
+#her seferinde bu hesaplamayı yapmamak adına değişken tanımlarız
+print(urunA +(urunA*kdv))
+print(urunB +(urunB*kdv))
+print(urunC +(urunC*kdv))
+#variebles rules
+"""
+sayi1 = 10, geçerli
+sayi2 = 20, geçerli
+3sayi hatalı olur
+_sayi3 = 30, geçerli
+sayi@ hatalı
+- eklenemez
+sayi5= 50,geçerli
+küçük büyük harfe duyarlı bir dil
+"""
+a,b,c=10,20,30
+print(a,b,c,)
+name = "Büşra" #string veri tipi
+print(name)
+
+isRight = True
+isWrong = False
+
+print(type(isWrong))
+
+a,b,c,name= 20,40,50,"berna"
+a= b+c
