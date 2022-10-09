9a165652370e43659e511d85c7ecdce221ca64db
Büşra Sümeyye <84714922+busrasumeyye@users.noreply.github.com>
2022-10-09 21:06:21 +0300

    Add files via upload

---
+++ python_temelleri/Demo1.py
@@ -0,0 +1,10 @@
+studentName ="Mine"
+studentLastName = "Erol"
+studentNameandLastName = studentName+' '+studentLastName
+studentId = "112233"
+studentGender = "Girl"
+studentTc= "1199223344"
+studentBirthYear = 1998
+studentAddress=" İstanbul Fenerbahçe"
+studentAge= 2022-studentBirthYear
+print(studentName,studentLastName,studentNameandLastName,studentId,studentGender,studentTc,studentBirthYear,studentAddress,studentAge)
---
+++ python_temelleri/Demo2.py
@@ -0,0 +1,5 @@
+urun1= 50
+urun2 = 60.5
+urun3 = 356.45
+
+print(urun1+urun2+urun3)
