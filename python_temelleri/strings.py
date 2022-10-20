fbdd544de806f8cb0f0316b027460b8f55aa6968
Büşra Sümeyye <84714922+busrasumeyye@users.noreply.github.com>
2022-10-20 23:09:26 +0300

    Add files via upload

---
+++ python_temelleri/strings.py
@@ -0,0 +1,9 @@
+name = 'busra'
+lastname = 'tatli'
+age = '23'
+character  ='b'
+msg = 'My name is ' + name + ' and my lastname is ' + lastname +"." + " My age is:" + age
+print(type(name))
+print(msg)
+print(msg[3])
+print(msg[-2])
